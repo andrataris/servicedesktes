16a7d461e56196524d9f288cdff441f2e5d13434
unknown <Andra@IA-NB-ETIK.silog.co.id>
2020-08-06 08:30:58 +0700

    test

---
+++ app/Http/Controllers/TraceticketController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TraceticketController extends Controller
+{
+    //
+    public function index() {
+
+        return view ('traceticket/viewticket');
+     }
+}
---
+++ resources/views/traceticket/viewticket.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Trace Ticket ServiceDesk</h3>
+            </div>
+    
+            <!-- SEARCH FORM -->
+    <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <table id="example2" class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                  <th>ID Ticket</th>
+                  <th>Tanggal</th>
+                  <th>Nopeg</th>
+                  <th>Nama</th>
+                  <th>Email</th>
+                  <th>Category</th>
+                  <th>Problem</th>
+                  <th>Status</th>
+                </tr>
+                </thead>
+                <tbody>
+            
+                <script>
+
+
+  <!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables -->
+<script src="../../plugins/datatables/jquery.dataTables.js"></script>
+<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+<!-- page script -->
+<script>
+  $(function () {
+    $("#example1").DataTable();
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+    });
+  });
+</script>
+
+@endsection
